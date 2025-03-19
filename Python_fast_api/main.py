5819ae88311968872b5ae8292d92d64310ccdef4
vinay-hegde17 <vinay.hegde@phyelements.com>
2025-03-19 10:49:05 +0530

    Body field examples

--- Python_fast_api/main.py
+++ Python_fast_api/main.py
@@ -1,7 +1,99 @@
-from fastapi import FastAPI
+# from typing import Annotated
+#
+# from fastapi import Body, FastAPI
+# from pydantic import BaseModel, Field
+#
+# app = FastAPI()
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = Field(
+#         default=None, title="The description of the item", max_length=300
+#     )
+#     price: float = Field(gt=0, description="The price must be greater than zero")
+#     tax: float | None = None
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+#     results = {"item_id": item_id, "item": item}
+#     return results
 
-app = FastAPI()
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+#
+# app = FastAPI()
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: list = []
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+# from typing import List, Union
+#
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+#
+# app = FastAPI()
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+#     tags: List[str] = []
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+#
+# app = FastAPI()
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: list = []
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+#
+# app = FastAPI()
+#
+#
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float | None = None
+#     tags: set[str] = set()
+#
+#
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     results = {"item_id": item_id, "item": item}
+#     return results
